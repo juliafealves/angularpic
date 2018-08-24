ad592c4d8f513e3b62e3b8beb7b65bc48efae4c0
juliafealves <juliafealves>
2018-08-24 15:53:52 -0300

    Modifiqued appComponent view.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularpic';
+  title = 'AngularPic';
+  picture = 'https://i.pinimg.com/originals/93/74/b0/9374b044c9b627c427f3f6ce2f139e96.jpg';
+  description = 'Lion';
 }
