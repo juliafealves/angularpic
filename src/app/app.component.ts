f9290ef7b45e3fbef58f55d46fb0a7b4e4e14f0a
juliafealves <juliafealves>
2018-08-27 21:58:19 -0300

    Added photos array to load photos in view.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPic';
+  photos = [{
+      url: 'https://i.pinimg.com/originals/93/74/b0/9374b044c9b627c427f3f6ce2f139e96.jpg',
+      description: 'Lion'
+    }, {
+      url: 'http://www.portaldosanimais.com.br/blog/wp-content/gallery/fotos-de-girafa/Fotos-de-Girafa-4.jpg',
+      description: 'Giraffe'
+    }];
 }
