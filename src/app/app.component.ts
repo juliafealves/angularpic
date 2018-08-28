3fe71ce903ec87ce1cf36113babe3ed5d4cbb95a
juliafealves <juliafealves>
2018-08-27 21:38:40 -0300

    Created app-photo component to create a responsive image.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPic';
-  picture = 'https://i.pinimg.com/originals/93/74/b0/9374b044c9b627c427f3f6ce2f139e96.jpg';
-  description = 'Lion';
 }
