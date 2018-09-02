a77448b80cefe454e86aa20f55484b6632ffce3c
juliafealves <juliafealves>
2018-09-01 21:51:46 -0300

    Created Not Fount component and module Errors.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 // Modules
 import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
 
 // Components
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
+        ErrorsModule,
         PhotosModule,
     ],
     providers: [],
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [{
         path: 'photos',
@@ -9,6 +11,9 @@ const routes: Routes = [{
     }, {
         path: 'photos/add',
         component: PhotoFormComponent
+    }, {
+        path: '**',
+        component: NotFoundComponent
     }];
 
 @NgModule({
