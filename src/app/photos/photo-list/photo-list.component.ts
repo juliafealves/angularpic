344f083e4cff8ad1ca708763e5d81079203f0870
juliafealves <juliafealves>
2018-09-07 16:31:21 -0300

    Added the design pattern "Debounce" in filter by photos.

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'ap-photo-list',
     templateUrl: './photo-list.component.html',
     styleUrls: [ './photo-list.component.css' ]
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
-    public photos: Photo[] = [];
+    public debounce: Subject<string> = new Subject<string>();
     public filter = '';
+    public photos: Photo[] = [];
 
     public constructor(private activatedRoute: ActivatedRoute) {
     }
 
     ngOnInit(): void {
         this.photos = this.activatedRoute.snapshot.data.photos;
+        this
+            .debounce
+            .pipe(debounceTime(300))
+            .subscribe(filter => this.filter = filter);
+    }
+
+    ngOnDestroy(): void {
+        this.debounce.unsubscribe();
     }
 }
