c5b1c584f05aeebb999e86e4685f41ba1b43eaef
juliafealves <juliafealves>
2018-09-10 23:41:55 -0300

    Created Card component for show content general formats.

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotoModule } from '../photo/photo.module';
+import { CardModule } from '../../shared/components/card/card.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        PhotoModule
+        PhotoModule,
+        CardModule
     ],
     declarations: [
         PhotoListComponent,
