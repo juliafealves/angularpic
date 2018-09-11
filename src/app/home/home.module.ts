fd823316280c46111d1d4d93e25926fc1a8842d3
juliafealves <juliafealves>
2018-09-11 20:48:11 -0300

    Created view input message error.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SignInComponent } from './sign-in/sign-in.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { SignInComponent } from './sign-in/sign-in.component';
+import { InputMessageModule } from '../shared/components/input-message/input-message.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        InputMessageModule
     ],
     declarations: [ SignInComponent ]
 })
